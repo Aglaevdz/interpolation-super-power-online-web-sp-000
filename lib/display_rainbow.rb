cceb61cee8a5704628ae59af8af7e0ee27810a9a
Aglae Verduzco <aglae.cv@gmail.com>
2018-10-10 01:17:35 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 def  display_rainbow(colors = "'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'")
   puts "display_rainbow(colors)"
 end
-display_rainbow
-display_rainbow"#{["R: red", "O: orange", "Y: yellow", "G: green", "B: blue", "I: indigo", "V: violet"]}"
+
+display_rainbow "R:#{[0]}, O:#{[1]}, Y:#{[2]}, G:#{[3]}, B:#{[4]}, I:#{[5]}, V:#{[6]}
